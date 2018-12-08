67f318fb0dcd172471d01b937330d47705b26db6
donalmshs@gmail.com <Manila1998>
2018-12-08 12:56:53 -0600

    Added a robber sprite to the game and have the robber sprite icon appearing ont he board

--- src/SimpleGame.py
+++ src/SimpleGame.py
@@ -12,7 +12,30 @@ import arcade
 
 # Setting game screen size as constant
 SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 700
+SCREEN_HEIGHT = 800
+
+
+class Robber:
+    robber = None
+
+    def __init__(self, position_x, position_y):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.sprite_scale = .3
+        self.robber_image = "sprites/robber.png"
+        self.robber_sprite = arcade.Sprite(self.robber_image, self.sprite_scale)
+
+    def draw(self):
+        self.robber_sprite.draw()
+
+    def animate(self):
+        pass
+
+    def get_scaling(self):
+        return 1
+
+    def get_robber_image(self):
+        return "sprites/robber.png"
 
 
 class SimpleGame(arcade.Window):
@@ -21,11 +44,13 @@ class SimpleGame(arcade.Window):
         # Calling the parent's init method
         super().__init__(width, height, title)
         # Setting the background color
-        arcade.set_background_color(arcade.color.BLUE_GRAY)
+        arcade.set_background_color(arcade.color.DONKEY_BROWN)
+        self.robber_list = arcade.SpriteList()
+        self.robber = Robber(300, 300)
 
     def on_draw(self):
         arcade.start_render()
-
+        self.robber.draw()
 
 def main():
     game_window = SimpleGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Game")
