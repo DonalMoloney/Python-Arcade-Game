77174de05ec1e1cb42b184f55f4c770489a4d474
donalmshs@gmail.com <Manila1998>
2019-01-08 20:09:29 -0600

    Attempting to get the sprites to animate

--- src/SimpleGame.py
+++ src/SimpleGame.py
@@ -9,73 +9,53 @@ __date__ = "12/7/18"
 """
 import random
 import sys
+from enum import Enum
 
 import arcade
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+
 
 # Setting game screen size as constant
-SCREEN_WIDTH = 700
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
+
+SPRITE_SCALING = 0.3
 
 
 # Todo abstract these classes into a character class
-class Sheriff:
-
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x
-        self.position_y = position_y
-        self.sprite_scale = .3
-        self.sheriff_image = "sprites/sheriff.png"
-        self.sheriff_sprite = arcade.Sprite(self.sheriff_image, self.sprite_scale)
-
-    def draw(self):
-        self.robber_sprite.draw()
-
-    def animate(self):
-        pass
-
-    def get_scaling(self):
-        return 1
-
-    def get_sheriff_image(self):
-        return "sprites/sheriff.png"
+class Direction(Enum):
+    LEFT = 0
+    RIGHT = 1
 
 
-class Robber:
+class Robber(arcade.Sprite):
+    ROBBER_SPEED = 50
+    CURRENT_DIRECTION = Direction.LEFT
 
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x
-        self.position_y = position_y
-        self.sprite_scale = .3
-        self.robber_image = "sprites/robber.png"
-        self.robber_sprite = arcade.Sprite(self.robber_image, self.sprite_scale)
+    def __init__(self, filename, sprite_scaling):
+        super().__init__(filename, sprite_scaling)
+        self.change_x = self.ROBBER_SPEED
 
-    def draw(self):
-        self.robber_sprite.draw()
-
-    def get_direction(number):
-        # Todo add other directions
-        if number == 0:
-            return "down"
+    def move_left(self):
+        if (self.center_x + self.ROBBER_SPEED) >= SCREEN_WIDTH:
+            self.center_x = SCREEN_WIDTH
+            self.CURRENT_DIRECTION = Direction.RIGHT  # Switch to other direction once you have hit the boundary
         else:
-            return "up"
+            self.center_x = self.center_x + self.ROBBER_SPEED
 
-    def animate(self):
-        self.move_sprite(self.get_direction(random.randint(0, 1)))
-
-    def get_scaling(self):
-        return 1
-
-    def get_robber_image(self):
-        return "sprites/robber.png"
-
-    def move_sprite(self, param):
-        if (param == "left") and (self.position_x != SCREEN_WIDTH):
-            self.position_x = self.position_x - 3
+    def move_right(self):
+        if 0 >= (self.center_x - self.ROBBER_SPEED):
+            self.center_x = SCREEN_WIDTH
+            self.CURRENT_DIRECTION = Direction.LEFT  # Switch to other direction once you have hit the boundary
         else:
-            self.position_x = self.position_x + 1
+            self.center_x = self.center_x - self.ROBBER_SPEED
+
+    def update(self):
+        print("UPDATE ENTERED")
+        if self.CURRENT_DIRECTION == Direction.LEFT:
+            self.move_left()
+        elif self.CURRENT_DIRECTION == Direction.RIGHT:
+            self.move_right()
 
 
 class SimpleGame(arcade.Window):
@@ -83,20 +63,40 @@ class SimpleGame(arcade.Window):
     def __init__(self, width, height, title):
         # Calling the parent's init method
         super().__init__(width, height, title)
+        self.robber_sprites_list = None
+
+    def start_new_game(self):
         # Setting the background color
         arcade.set_background_color(arcade.color.DONKEY_BROWN)
-        self.robber_list = arcade.SpriteList()
-        self.robber = Robber(300, 300)
-        self.sheriff = Sheriff(300, 300)
+        self.robber_sprites_list = arcade.SpriteList()
+        # Creating instance of robber class
+        for i in range(3):
+            # create Robber instance
+            robber_sprite = Robber("./sprites/robber.png", SPRITE_SCALING)
+
+            # Position the sprite
+            robber_sprite.center_x = random.randrange(SCREEN_WIDTH)
+            robber_sprite.center_y = 400 + random.randrange(400)  # TODO comment this
+
+            # Add the robber sprite to the list of sprites
+            self.robber_sprites_list.append(robber_sprite)
+            self.robber_sprites_list.update_animation(2)
 
     def on_draw(self):
+        # Command that must be called before drawing can occur
         arcade.start_render()
-        self.robber.draw()
-       # self.sheriff.draw()
+
+        # Draw all the robbers in the list
+        self.robber_sprites_list.draw()
 
     def on_key_press(self, key, modifiers):
         pass
-        # todo
+        # todo implement this when implementing the sheriff
+
+    def animate(self, delta_time):
+        # Call update on all sprites (The sprites don't do much in this #
+        print("Entererd")
+        self.robber_sprites_list.update()
 
 
 class App(QWidget):
@@ -113,11 +113,12 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        buttonReply = QMessageBox.question(self, 'Game Play Message',
-                                           "Do you want to play Sheriff Jones Robber Roundup?",
-                                           QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-        if buttonReply == QMessageBox.Yes:
+        button_reply = QMessageBox.question(self, 'Game Play Message',
+                                            "Do you want to play Sheriff Jones Robber Roundup?",
+                                            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if button_reply == QMessageBox.Yes:
             game_window = SimpleGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Game")
+            game_window.start_new_game()
             arcade.run()
         else:
             self.close()
